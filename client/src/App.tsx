920b9ca31d1eb93d3197912001dec6841be257d5
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-11-18 16:11:59 +0200

    Improve weather component separation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,10 @@
-import { useWeather } from './hooks/useWeather';
-import { useGeoLocation } from './hooks/useGeoLocation';
 import Weather from './components/weather';
 
 const App = () => {
-  const coords = useGeoLocation();
-  const weather = useWeather(coords);
-
   return (
     <div>
       <h1>Weather info</h1>
-      <Weather weather={weather.weather} loading={weather.loading} error={weather.error} />
+      <Weather />
     </div>
   );
 };
--- client/src/components/weather.tsx
+++ client/src/components/weather.tsx
@@ -1,27 +1,32 @@
-import type { UseWeatherReturn } from '../../../shared/types/weather';
+import { useWeather } from '../hooks/useWeather';
+import { useGeoLocation } from '../hooks/useGeoLocation';
 
-const Weather = ({ weather, loading, error }: UseWeatherReturn) => {
-  if (loading) {
+const Weather = () => {
+  const coords = useGeoLocation();
+  const weather = useWeather(coords);
+  const weatherData = weather.weather;
+
+  if (weather.loading) {
     return <p>Loading weather data...</p>;
   }
 
-  if (error) {
-    return <p>Error loading weather: {error}</p>;
+  if (weather.error) {
+    return <p>Error loading weather: {weather.error}</p>;
   }
 
-  if (!weather) {
+  if (!weatherData) {
      return <p>No weather data available</p>;
   }
 
   return (
     <div>
-      <p>Weather in {weather.city}:</p>
+      <p>Weather in {weatherData.city}:</p>
       <ul>
-        <li>Main: {weather.main}</li>
-        <li>Temperature: {weather.temperature}°C</li>
-        <li>Cloudiness: {weather.cloudiness}%</li>
-        <li>Sunrise: {new Date(weather.sunrise * 1000).toLocaleTimeString('fi-FI')}</li>
-        <li>Sunset: {new Date(weather.sunset * 1000).toLocaleTimeString('fi-FI')}</li>
+        <li>Main: {weatherData.main}</li>
+        <li>Temperature: {weatherData.temperature}°C</li>
+        <li>Cloudiness: {weatherData.cloudiness}%</li>
+        <li>Sunrise: {new Date(weatherData.sunrise * 1000).toLocaleTimeString('fi-FI')}</li>
+        <li>Sunset: {new Date(weatherData.sunset * 1000).toLocaleTimeString('fi-FI')}</li>
       </ul>
     </div>
   );
