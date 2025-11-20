50fe59c07c6b87abda3ad285808ead8bed7ef0c8
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-11-20 16:15:26 +0200

    Create reducer for artworks data

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import weatherReducer from './reducers/weatherSlice';
 import keywordReducer from './reducers/keywordSlice';
+import artworkReducer from './reducers/artworkSlice';
 
 export const store = configureStore({
   reducer: {
     weather: weatherReducer,
     keywords: keywordReducer,
+    artworks: artworkReducer,
   }
 });
 
