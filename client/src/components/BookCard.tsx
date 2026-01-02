0250aaae52fb82aa079a845ed2dc3851c1b348e1
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2026-01-02 14:04:40 +0200

    Add link to the book, reformat text

--- client/src/components/BookCard.tsx
+++ client/src/components/BookCard.tsx
@@ -14,18 +14,21 @@ const BookCard = ({ book }: BookCardProps) => {
 
   return (
     <div className="bg-[var(--color-dark)] block max-w-sm rounded-2xl shadow-xs">
-      <img 
+      <a href={`https://openlibrary.org${book.id}`} target="_blank" rel="noopener noreferrer">
+        <img 
         className="rounded-t-2xl object-cover w-full" 
         src={proxiedImageUrl} 
         alt={book.title}
         onError={handleImageError}
-        loading="lazy"
       />
+      </a>
       <div className="p-6">
         <h5 className="mt-1 mb-1 font-semibold tracking-tight text-heading">
-          {book.title} <br />
-          {book.authors.join(', ')}, {book.year} <br />
+          <a href={`https://openlibrary.org${book.id}`} target="_blank" rel="noopener noreferrer">{book.title}</a>
         </h5>
+        <p>
+          {book.authors.join(', ')}, {book.year}
+        </p>
       </div>
     </div>
   );
