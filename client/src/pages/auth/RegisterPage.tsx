ee54501bc99e19a7058dd7e83a31424ba3073dc5
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2026-01-12 16:27:03 +0200

    Create place holder page for registration

---
+++ client/src/pages/auth/RegisterPage.tsx
@@ -0,0 +1,12 @@
+const RegisterPage = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="w-96">
+        <h1>Create account</h1>
+        {/* form */}
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
